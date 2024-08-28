1123be2b6d4420984f4c02de64171cfc1ba2a115
Mark <mhrren93@gmail.com>
2024-08-29 00:48:55 +0100

    Fixes and updates, add player/user, create player/team/user
    Signed-off-by: Mark <mhrren93@gmail.com>

--- src/main/java/mark/warren93/dev/WarriorsFootballAssociationapi/WarriorsFootballAssociationApiApplication.java
+++ src/main/java/mark/warren93/dev/WarriorsFootballAssociationapi/WarriorsFootballAssociationApiApplication.java
@@ -10,19 +10,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 @ComponentScan
 public class WarriorsFootballAssociationApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WarriorsFootballAssociationApiApplication.class, args);
 	}
-
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
+				registry.addMapping("/api/v1/**")
 						.allowedOrigins("*")
-						.allowedMethods("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
 						.allowedHeaders("*")
 						.allowCredentials(false).maxAge(3600);
 			}
