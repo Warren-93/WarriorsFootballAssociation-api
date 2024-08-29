92d2c0d0f77da5497db59826baa035baa14b7238
Mark <mhrren93@gmail.com>
2024-08-29 03:55:43 +0100

    Fixes and updates, add player/user, create player/team/user
    Signed-off-by: Mark <mhrren93@gmail.com>

--- src/main/java/mark/warren93/dev/WarriorsFootballAssociationapi/model/Team.java
+++ src/main/java/mark/warren93/dev/WarriorsFootballAssociationapi/model/Team.java
@@ -1,9 +1,7 @@
 package mark.warren93.dev.WarriorsFootballAssociationapi.model;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,22 +11,16 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class Team {
     @Id
-    private ObjectId ObjectId;
+    private String ObjectId;  // Change ObjectId to String
     private int teamId;
     private String teamName;
     private List<Player> players;
+
     public Team(int teamId, String teamName, List<Player> players) {
-        this.teamId = teamId+=1;
+        this.teamId = teamId + 1;
         this.teamName = teamName;
         this.players = players;
-    }
-    public ObjectId getObjectId() {
-        return ObjectId;
-    }
-    public void setObjectId(ObjectId objectId) {
-        ObjectId = objectId;
     }
 }
