61515017c93b77754467cb5c9a8d56259301d8bc
Mark <mhrren93@gmail.com>
2024-09-03 00:58:57 +0100

    Login/Register
    Signed-off-by: Mark <mhrren93@gmail.com>

--- src/main/java/mark/warren93/dev/WarriorsFootballAssociationapi/service/UserAuthService.java
+++ src/main/java/mark/warren93/dev/WarriorsFootballAssociationapi/service/UserAuthService.java
@@ -24,8 +24,9 @@ public class UserAuthService {
         System.out.println("AuthLogin");
         User user = userService.getUserByUsername(username);
 
-
+        System.out.println(user.toString());
         if (user != null && password.matches(user.getPassword().toString())){
+            System.out.println(user.getPassword().toString());
             // Generate JWT Token
             return Jwts.builder()
                     .setSubject(user.getUsername())  // Use username as the subject
@@ -35,6 +36,7 @@ public class UserAuthService {
                     //.signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                     .compact();
         } else {
+            System.out.print("User is null");
             throw new IllegalArgumentException("Invalid Credentials");
         }
     }
